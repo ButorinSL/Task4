9cbc7a64decad75dd3ef4d223defa05feede3c33
Sergey_Butorin <ButorinSL@gmail.com>
2023-03-31 22:29:36 +0500

    Ошибка сборки

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+//
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число А->");
+        string valueA; // Объявление переменной типа строка
+        valueA = Console.ReadLine(); // Считываем с консоли строку
+        int numberA = Convert.ToInt32(valueA); // переводим введеное значение в числовой режим
+        int numberX = 1;
+
+        while (numberX < numberA);
+
+            int ostatok = numberA % 2;
+            if (ostatok == 0)
+            {
+                Console.WriteLine (numberX);
+            }
+        numberX = numberX + 1;
+    }
+}
+
